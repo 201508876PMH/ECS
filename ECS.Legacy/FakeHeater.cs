036651857a2fd0dbabc40c6988f5a84fac870932
Oliver Monberg <au554208@uni.au.dk>
2018-09-11 10:01:43 +0200

    Constructor injection

---
+++ ECS.Legacy/FakeHeater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    internal class FakeHeater : IHeater
+    {
+        public void TurnOn()
+        {
+            System.Console.WriteLine("FakeHeater is on");
+        }
+
+        public void TurnOff()
+        {
+            System.Console.WriteLine("FakeHeater is off");
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
---
+++ ECS.Legacy/FakeTempSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Legacy
+{
+    internal class FakeTempSensor : ITempSensor
+    {
+       //private Random gen = new Random();
+
+        public int GetTemp()
+        {
+            return 25;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
